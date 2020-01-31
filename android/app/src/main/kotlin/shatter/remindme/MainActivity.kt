be253f2c5a24cb99adcabb40dd55f71fb9273050
Abhishek Kedia <iamabhikedia74@gmail.com>
2020-02-01 01:22:26 +0530

    Map error fixed

--- android/app/src/main/kotlin/shatter/remindme/MainActivity.kt
+++ android/app/src/main/kotlin/shatter/remindme/MainActivity.kt
@@ -1,9 +1,10 @@
 package shatter.remindme
 
-import android.support.annotation.NonNull;
+
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,58 @@
-import 'package:flutter/material.dart';
-import './homepage.dart';
+//import 'package:flutter/material.dart';
+//import './homepage.dart';
+//
+//void main() {
+//  runApp(Start());
+//}
+//
+//class Start extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: "RemindME",
+//      debugShowCheckedModeBanner: false,
+//      home: new Container(
+//        child: HomePage(),
+//      ),
+//    );
+//  }
+//}
 
-void main() {
-  runApp(Start());
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class Start extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "RemindME",
-      debugShowCheckedModeBanner: false,
-      home: new Container(
-        child: HomePage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       ),
     );
   }
