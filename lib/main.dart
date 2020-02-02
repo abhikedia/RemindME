4d713c52b15a4ac241b694a5795140397bb6a9d9
Abhishek Kedia <iamabhikedia74@gmail.com>
2020-02-02 16:20:45 +0530

    Current location marker added

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@
 //}
 
 import 'dart:async';
+import 'dart:math';
 import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -56,10 +57,92 @@ class _MyAppState extends State<MyApp> {
 //    });
   }
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+//LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  static final LatLng center = const LatLng(-33.86711, 151.1947171);
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  MarkerId selectedMarker;
+  int _markerIdCounter = 2;
+
+  void _add() {
+    final int markerCount = markers.length;
+
+    if (markerCount == 12) {
+      return;
+    }
+
+    final String markerIdVal = 'marker_id_$_markerIdCounter';
+    _markerIdCounter++;
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(
+        center.latitude + sin(_markerIdCounter * pi / 6.0) / 20.0,
+        center.longitude + cos(_markerIdCounter * pi / 6.0) / 20.0,
+      ),
+      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+      onTap: () {
+        _onMarkerTapped(markerId);
+      },
+      onDragEnd: (LatLng position) {
+        _onMarkerDragEnd(markerId, position);
+      },
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  void _onMarkerTapped(MarkerId markerId) {
+    final Marker tappedMarker = markers[markerId];
+    if (tappedMarker != null) {
+      setState(() {
+        if (markers.containsKey(selectedMarker)) {
+          final Marker resetOld = markers[selectedMarker]
+              .copyWith(iconParam: BitmapDescriptor.defaultMarker);
+          markers[selectedMarker] = resetOld;
+        }
+        selectedMarker = markerId;
+        final Marker newMarker = tappedMarker.copyWith(
+          iconParam: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen,
+          ),
+        );
+        markers[markerId] = newMarker;
+      });
+    }
+  }
+
+  void _onMarkerDragEnd(MarkerId markerId, LatLng newPosition) async {
+    final Marker tappedMarker = markers[markerId];
+    if (tappedMarker != null) {
+      await showDialog<void>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+                actions: <Widget>[
+                  FlatButton(
+                    child: const Text('OK'),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )
+                ],
+                content: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 66),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text('Old position: ${tappedMarker.position}'),
+                        Text('New position: $newPosition'),
+                      ],
+                    )));
+          });
+    }
   }
 
   @override
@@ -70,33 +153,41 @@ class _MyAppState extends State<MyApp> {
           title: Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-//        body: GoogleMap(
-//          onMapCreated: _onMapCreated,
-//          initialCameraPosition: CameraPosition(
-//            target: _center,
-//            zoom: 11.0,
-//          ),
-//        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Lat/Lng:${currentLocation['latitude']}/${currentLocation['longitude']}',
-                style: TextStyle(fontSize: 20.0, color: Colors.black),
-              )
-            ],
-          ),
+        body: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: center,
+              zoom: 11.0,
+            ),
+            rotateGesturesEnabled: true,
+            scrollGesturesEnabled: true,
+            tiltGesturesEnabled: true,
+            markers: Set<Marker>.of(markers.values)
+        ),
+        floatingActionButton: new FloatingActionButton(
+          child: Icon(Icons.my_location),
+          onPressed: () {
+            mapController.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                    target: LatLng(currentLocation['latitude'],
+                        currentLocation['longitude']),
+                    zoom: 20.0),
+              ),
+            );
+          },
         ),
       ),
     );
   }
+
 
   void initPlatformState() async {
     LocationData my_location;
     try {
       my_location = await location.getLocation();
       print(my_location.latitude);
+      print(my_location.longitude);
       error = "";
     } on PlatformException catch (e) {
       if (e.code == 'PERMISSION_DENIED')
@@ -108,6 +199,12 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       currentLocation['latitude'] = my_location.latitude;
       currentLocation['longitude'] = my_location.longitude;
+      markers[MarkerId('1')] = Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(
+            my_location.latitude, my_location.longitude
+        ),
+      );
     });
   }
 }
